6268ce6656055f9dc8393d8b8c1aeda1ee081de7
JefersonFG <jefersonfguimaraes@tutanota.com>
2022-04-17 17:02:37 -0300

    Day 2 Part 1, added cli arg parsing for input path

---
+++ Day 2 - Dive/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// Open input file
+	var input_file_path string
+	flag.StringVar(&input_file_path, "input_path", "input.txt", "path to the input file, with one command on each line")
+	flag.Parse()
+	input_file, err := os.Open(input_file_path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer input_file.Close()
+
+	// Create scanner to read file line by line
+	scanner := bufio.NewScanner(input_file)
+
+	// Read each input line into a slice of commands and one of offsets
+	var commands []string
+	var offsets []int64
+
+	for scanner.Scan() {
+		// Reads a line and check that it isn't empty
+		line := scanner.Text()
+		if len(line) == 0 {
+			break
+		}
+
+		// Splits the line between the command and the value
+		input_values := strings.Split(line, " ")
+
+		command, offset := input_values[0], input_values[1]
+
+		converted_offset, err := strconv.ParseInt(offset, 10, 64)
+		if err != nil {
+			log.Fatal(err)
+			panic(err)
+		}
+
+		// Saves the parsed input
+		commands = append(commands, command)
+		offsets = append(offsets, converted_offset)
+	}
+
+	// Checks for scanner errors, panics if one is found
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	// ### Part 1 ###
+	// Submarine position
+	horizontal_position := 0
+	depth := 0
+
+	// Traverse the list of instructions operating on the position values
+	for i := 0; i < len(commands); i++ {
+		// Checks for current command
+		// Assumes only valid commands
+		switch commands[i] {
+		case "forward":
+			horizontal_position += int(offsets[i])
+		case "down":
+			depth += int(offsets[i])
+		default:
+			depth -= int(offsets[i])
+		}
+	}
+
+	// Prints the result
+	fmt.Printf("Final position value: %d\n", horizontal_position*depth)
+}
