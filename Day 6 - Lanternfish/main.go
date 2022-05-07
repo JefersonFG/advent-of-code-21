9fcd63ada6ad73a441f389648087c85b2cced778
JefersonFG <jefersonfguimaraes@tutanota.com>
2022-05-07 10:24:02 -0300

    Day 6 pt 1 - 80 days simulation

--- Day 6 - Lanternfish/main.go
+++ Day 6 - Lanternfish/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 	"strings"
+)
+
+// Constants
+const (
+	simulation_days         = 80
+	lanternfish_reset_value = 6
+	new_lanternfish_value   = 8
 )
 
 func main() {
@@ -51,5 +59,27 @@ func main() {
 		panic(err)
 	}
 
-	// TODO: Simulate the lanternfish evolution
+	// Simulate specific number of days of lanternfish evolution
+	for day := 0; day < simulation_days; day++ {
+		// Saves the current number of lanternfishes so as to not calculate the evolution of new ones
+		school_size := len(lanternfishes)
+
+		// Calculate the evolution for each lanternfish that started the day
+		for i := 0; i < school_size; i++ {
+			// If the counter is zero spawn a new lanternfish and reset the counter
+			if lanternfishes[i] == 0 {
+				// Reset the counter
+				lanternfishes[i] = lanternfish_reset_value
+
+				// Add new fish
+				lanternfishes = append(lanternfishes, new_lanternfish_value)
+			} else {
+				// Otherwise just decrease the counter
+				lanternfishes[i]--
+			}
+		}
+	}
+
+	// Print number of lanternfishes
+	fmt.Println("Number of lanternfishes at the end of the simulation:", len(lanternfishes))
 }
