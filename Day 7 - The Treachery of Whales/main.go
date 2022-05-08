dcf85afa83da8e9297c94393636860eef223bef1
JefersonFG <jefersonfguimaraes@tutanota.com>
2022-05-08 10:53:29 -0300

    Day 7 pt 2 - Mean values with two candidates

--- Day 7 - The Treachery of Whales/main.go
+++ Day 7 - The Treachery of Whales/main.go
@@ -13,6 +13,10 @@ import (
 )
 
 func main() {
+	// Parameter for calculating with a linear fuel consumption rate or not
+	var linear_consumption bool
+	flag.BoolVar(&linear_consumption, "linear_consumption", true, "calculate with a linear consumption rate or increasing consumption rate")
+
 	// Open input file
 	var input_file_path string
 	flag.StringVar(&input_file_path, "input_path", "sample_input.txt", "path to the input file, with one command on each line")
@@ -54,14 +58,38 @@ func main() {
 		panic(err)
 	}
 
-	// Calculate the optimal horizontal position
-	// The mean value gets very skewed by outliers, so we use the median value instead
-	optimal_position := median(horizontal_positions)
-
-	// Calculate the fuel consumption to get there
 	fuel_consumption := 0
-	for _, position := range horizontal_positions {
-		fuel_consumption += int(math.Abs(float64(position - optimal_position)))
+
+	if linear_consumption {
+		// In the case of linear fuel consumption the mean value gets very skewed by outliers
+		// So we use the median value instead
+		optimal_position := median(horizontal_positions)
+
+		for _, position := range horizontal_positions {
+			fuel_consumption += int(math.Abs(float64(position - optimal_position)))
+		}
+	} else {
+		// In the case of non linear fuel consumption we have two candidates for the optimal position
+		// So we check both for the lower fuel consumption
+		// Also here the outliers are important as they mean more fuel consumption, so we use the mean
+		ceiled_position := meanCeiled(horizontal_positions)
+		floored_position := meanFloored(horizontal_positions)
+
+		ceiled_consumption := 0
+		floored_consumption := 0
+
+		for _, position := range horizontal_positions {
+			distance := int(math.Abs(float64(position - ceiled_position)))
+			ceiled_consumption += (distance * (distance + 1)) / 2
+			distance = int(math.Abs(float64(position - floored_position)))
+			floored_consumption += (distance * (distance + 1)) / 2
+		}
+
+		if ceiled_consumption < floored_consumption {
+			fuel_consumption = ceiled_consumption
+		} else {
+			fuel_consumption = floored_consumption
+		}
 	}
 
 	fmt.Printf("Fuel consumption: %d\n", fuel_consumption)
@@ -75,4 +103,24 @@ func median(values []int) int {
 	} else {
 		return (values[len(values)/2-1] + values[len(values)/2]) / 2
 	}
+}
+
+// Function for calculating the mean value ceiling the result
+func meanCeiled(values []int) int {
+	sum := 0
+	num_values := float64(len(values))
+	for _, value := range values {
+		sum += value
+	}
+	return int(math.Ceil(float64(sum) / num_values))
+}
+
+// Function for calculating the mean value flooring the result
+func meanFloored(values []int) int {
+	sum := 0
+	num_values := float64(len(values))
+	for _, value := range values {
+		sum += value
+	}
+	return int(math.Floor(float64(sum) / num_values))
 }
